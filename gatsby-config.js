91a4b33d83296ce954f26d81f7fde02d16255a19
Davejs136 <davejs136@gmail.com>
2020-11-03 14:14:30 -0400

    add: intl-graphql source-strapi plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,20 @@
+require('dotenv').config({
+  path: 'env'
+})
+
+const query = require('./queries')
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Multi Language Site`,
+    description: `A simple multi language site made with Gatsby and intl-graphql.`,
+    author: `@Davejs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +22,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -24,11 +31,27 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-strapi',
+      options: {
+        apiURL: process.env.API_URL,
+        singleTypes: ['principal'],
+        queryLimit: 1000,
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-intl-graphql',
+      options: {
+        url: process.env.API_URL,
+        path: `${__dirname}/src/lang`,
+        languages: ['en', 'es'],
+        defaultLanguage: 'es',
+        redirect: true,
+        query,
+      }
+    }
   ],
 }

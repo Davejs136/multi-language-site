2f7009efc890797efc3d88307a9d29a88ad87e02
Davejs136 <davejs136@gmail.com>
2020-11-03 16:41:22 -0400

    change image and import intl-graphql

--- src/components/image.js
+++ src/components/image.js
@@ -16,17 +16,19 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+      strapiPrincipal {
+        imagen {
+          childImageSharp {
+            fluid(maxWidth: 500) {
+              ...GatsbyImageSharpFluid
+            }
           }
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return <Img fluid={data.strapiPrincipal.imagen.childImageSharp.fluid} />
 }
 
 export default Image

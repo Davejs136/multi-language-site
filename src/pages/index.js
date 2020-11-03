5ecc770b86c873aa64aefa660da72c1516e8629b
Davejs136 <davejs136@gmail.com>
2020-11-03 16:31:00 -0400

    feat: multi language support
    feat: page in multi language
    
    add: lang folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,26 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useIntl } from "../../plugins/gatsby-plugin-intl-graphql"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const {
+    messages: { principal }
+  } = useIntl()
+
+  console.log(principal)
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{principal.content.principal.titulo}</h1>
+      <p>{principal.content.principal.descripcion}</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
